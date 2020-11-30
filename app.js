955d148f4f449d3a23c47321f68bd249e071cf4e
Sunil Buddala <sunil@donatekart.com>
2020-11-30 23:39:09 +1300

    use print media type

--- app.js
+++ app.js
@@ -17,11 +17,16 @@ app.get("/pdf", async function (req, res) {
       waitUntil: "networkidle2",
     });
     if (pageResponse.ok()) {
-      const pdf = await page.pdf();
+      await page.emulateMediaType("print");
+      const pdf = await page.pdf({
+        format: "A4",
+      });
       res.writeHead(200, { "Content-Type": "application/pdf" });
       res.end(pdf);
     } else {
-      res.status(500).send("Failed to generaete pdf. " + await pageResponse.text());
+      res
+        .status(500)
+        .send("Failed to generaete pdf. " + (await pageResponse.text()));
     }
     await browser.close();
   } catch (e) {
